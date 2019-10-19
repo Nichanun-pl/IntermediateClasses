9815c8a6c6c9850fdbdd2e8f0be5f2cb4b808f46
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 03:16:46 +0700

    AddCustomer

---
+++ 2.Classes/2-2. Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Customer
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
