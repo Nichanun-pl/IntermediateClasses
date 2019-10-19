4289b8fbce77a5ba165c8e6f1f1268c253a60d01
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 04:50:26 +0700

    update

--- 2.Classes/2-6. Access Modifiers/Program.cs
+++ 2.Classes/2-6. Access Modifiers/Program.cs
@@ -5,6 +5,16 @@ namespace Access_Modifiers
     public class Person
     {
         private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
     }
     class Program
     {
