c3b5bd486824ac737d59ae667644db5a8142bd4d
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 04:03:29 +0700

    Update

--- 2.Classes/2-4. Methods/Point.cs
+++ 2.Classes/2-4. Methods/Point.cs
@@ -11,5 +11,16 @@
             this.Y = y;
         }
 
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            this.X = newLocation.X;
+            this.Y = newLocation.Y;
+        }
     }
 }
