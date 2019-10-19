0d36c675e3b8c219008748d2866de549abb0b9ea
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 02:27:37 +0700

    Update

--- 2.Classes/2-1.Introduction to Classes/Program.cs
+++ 2.Classes/2-1.Introduction to Classes/Program.cs
@@ -7,14 +7,16 @@ namespace Classes
         public string Name;
         public void Introduce(string to)
         {
-            Console.WriteLine("Hi {0}), I am {1}", to, Name);
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
         }
     }
         class Program
     {
         static void Main(string[] args)
         {
-            
+            var person = new Person();
+            person.Name = "John";
+            person.Introduce("Mosh");
         }
     }
 }
