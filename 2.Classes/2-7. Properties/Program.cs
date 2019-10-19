88b724f8d8552fbbc34a2fa74a4df2b9f21ba110
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 05:11:35 +0700

    update

--- 2.Classes/2-7. Properties/Program.cs
+++ 2.Classes/2-7. Properties/Program.cs
@@ -2,6 +2,21 @@
 
 namespace Properties
 {
+    public class Person
+    {
+        public DateTime Birthdate { get; private set; }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
