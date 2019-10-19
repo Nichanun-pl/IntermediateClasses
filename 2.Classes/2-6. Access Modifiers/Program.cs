e8a9fad5192195a408898ae3548259503623ddac
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 04:51:57 +0700

    2-6 Finish

--- 2.Classes/2-6. Access Modifiers/Program.cs
+++ 2.Classes/2-6. Access Modifiers/Program.cs
@@ -21,6 +21,8 @@ namespace Access_Modifiers
         static void Main(string[] args)
         {
             var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
         }
     }
 }
