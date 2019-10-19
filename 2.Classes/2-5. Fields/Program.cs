cdaadecc7b49743dcc8a4dc939db6ca9a141fceb
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 04:32:12 +0700

    create

---
+++ 2.Classes/2-5. Fields/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+    }
+
+    public class Order
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
