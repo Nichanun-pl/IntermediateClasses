324e585fb3f07be1e1426075fb79a705242adeb3
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 03:40:43 +0700

    Update

--- 2.Classes/2-2. Constructors/Customer.cs
+++ 2.Classes/2-2. Constructors/Customer.cs
@@ -4,5 +4,16 @@
     {
         public int Id;
         public string Name;
+
+        public Customer(int Id)
+        {
+            this.Id = Id;
+        }
+
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
     }
 }
