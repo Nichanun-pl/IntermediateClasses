8f4ced0f6ef6a416e83d1a0d2a3d4efed3e0761d
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-21 00:09:48 +0700

    6-5 Add

---
+++ 6. Interfaces/5.Polymorphism/VideoEncoder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class VideoEncoder
+    {
+        private readonly MailService mailService;
+
+        public VideoEncoder()
+        {
+            _mailService = new MailService();
+        }
+
+        public void Encode(Video video)
+        {
+            //Video encoding logic
+            //...
+
+            _mailService.Send(new Mail());
+        }
+    }
+}
