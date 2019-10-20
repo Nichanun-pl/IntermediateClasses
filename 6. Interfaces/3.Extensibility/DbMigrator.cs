db0e7fe5e2746ffef8840a661c244e0126827697
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 21:58:17 +0700

    6-3 Add

---
+++ 6. Interfaces/3.Extensibility/DbMigrator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Extensibility
+{
+    public class DbMigrator
+    {
+        public void Migrator()
+        {
+            Console.WriteLine("Migrationg started at {0}", DateTime.Now);
+            
+            //Details of migrating the database
+            
+            Console.WriteLine("Migrationg finished at {0}", DateTime.Now);
+        }
+    }
+}
