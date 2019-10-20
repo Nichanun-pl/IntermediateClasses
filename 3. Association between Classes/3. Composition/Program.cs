584b841a716bc6ee81e3fb8726f0431b1e4d270a
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 15:12:05 +0700

    update

--- 3. Association between Classes/3. Composition/Program.cs
+++ 3. Association between Classes/3. Composition/Program.cs
@@ -2,6 +2,14 @@
 
 namespace Composition
 {
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
