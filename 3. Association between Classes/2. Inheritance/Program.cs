f86b5cf23b6ba8a3d154e75707db2b8c548f36fd
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 15:04:03 +0700

    3-2 Finish

--- 3. Association between Classes/2. Inheritance/Program.cs
+++ 3. Association between Classes/2. Inheritance/Program.cs
@@ -1,23 +1,13 @@
-﻿using System;
-
-namespace Inheritance
+﻿namespace Inheritance
 {
-    public class Text : PresentationObject
-    {
-        public int FontSize { get; set; }
-        public string FontName { get; set; }
-
-        public void AddHyperlink(string url)
-        {
-            Console.WriteLine("We added a link to " + url);
-        }
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
         }
     }
 }
