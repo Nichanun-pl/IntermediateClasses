620d77c1f0713ebbb5a352fc8364566fa09ca825
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 15:45:03 +0700

    Add

---
+++ 4. Inheritance - Second Pillar of OOP/1. Access Modifiers/Access Modifiers/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Access_Modifiers
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promoto()
+        {
+            var rating = CalculateRating(excludeOrders: true);
+            if (rating == 0)
+                Console.WriteLine("Promoto to Level 1");
+            else
+                Console.WriteLine("Promoto to Level 2");
+        }
+
+        private int CalculateRating(bool excludeOrders)
+        {
+            return 0;
+        }
+
+    }
+}
