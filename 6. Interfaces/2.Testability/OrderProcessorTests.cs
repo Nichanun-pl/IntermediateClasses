a03716bc304b992ef69c38a3205320ee423c21c3
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 21:28:17 +0700

    6-2 create

---
+++ 6. Interfaces/2.Testability/OrderProcessorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testability.UnitTest
+{
+    [TestClass]
+    public class OrderProcessorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
