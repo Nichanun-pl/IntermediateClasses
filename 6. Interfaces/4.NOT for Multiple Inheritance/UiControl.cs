cf853d81546d16468854f272ef1611a118764954
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 23:39:58 +0700

    6-4 update

--- 6. Interfaces/4.NOT for Multiple Inheritance/UiControl.cs
+++ 6. Interfaces/4.NOT for Multiple Inheritance/UiControl.cs
@@ -2,6 +2,19 @@
 
 namespace Multiple_Inheritance
 {
+    public class TextBox : UiControl, IDraggable, IDroppable
+    {
+        public void Drag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Drop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class UiControl
     {
         public string Id { get; set; }
