314a668374e291827813aa91bbfcbc7dbdbf6561
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 15:37:57 +0700

    update

--- 4. Inheritance - Second Pillar of OOP/1. Access Modifiers/Access Modifiers/Program.cs
+++ 4. Inheritance - Second Pillar of OOP/1. Access Modifiers/Access Modifiers/Program.cs
@@ -2,11 +2,22 @@
 
 namespace Access_Modifiers
 {
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promoto()
+        {
+            
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var customer = new Customer();
         }
     }
 }
