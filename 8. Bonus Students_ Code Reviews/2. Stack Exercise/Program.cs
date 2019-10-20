6ebff676f82741fcfc98259047b77b3e026c13e9
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-21 01:49:10 +0700

    8-2 Finish

--- 8. Bonus Students_ Code Reviews/2. Stack Exercise/Program.cs
+++ 8. Bonus Students_ Code Reviews/2. Stack Exercise/Program.cs
@@ -7,23 +7,25 @@ namespace CSharpIntermediate
     {
         static void Main(string[] args)
         {
-            Stack _stack = new Stack();
+            var stack = new Stack();
 
             int ini = 0;
             int max = 5;
 
             for (int i = ini; i <= max; i++)
-                _stack.Push(i);
+                stack.Push(i);
+
+            stack.Clear();
 
             for (int i = ini; i <= max; i++)
-                Console.WriteLine(_stack.Pop());
+                Console.WriteLine(stack.Pop());
 
             Console.ReadLine();
         }
     }
     public class Stack
     {
-        private List<object> _list = new List<object>();
+        private readonly List<object> _list = new List<object>();
 
         public void Push(object obj)
         {
@@ -31,17 +33,24 @@ namespace CSharpIntermediate
                 throw new InvalidOperationException("You cann't add a null object");
 
             _list.Add(obj);
+
         }
         public object Pop()
         {
             if (_list.Count == 0)
                 throw new InvalidOperationException("No element in the stack yet");
 
-            object ToReturn = _list[_list.Count - 1];
+            var index = _list.Count - 1;
 
-            _list.RemoveAt(_list.Count - 1);
+            var ToReturn = _list[index];
+
+            _list.Remove(_list.Count - 1);
 
             return (ToReturn);
+        }
+        public void Clear()
+        {
+            _list.Clear();
         }
     }
 }
