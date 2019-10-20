70121f4019bc4b3f586d96c537a89b2c0e2a34e9
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-21 00:26:41 +0700

    Revert "6-5 update"
    This reverts commit d31f1c2a63bec276b521ffd18f742d754618765b.

--- 6. Interfaces/5.Polymorphism/INotficationChannel.cs
+++ 6. Interfaces/5.Polymorphism/INotficationChannel.cs
@@ -4,14 +4,6 @@ namespace Polymorphism
 {
     public interface INotificationChannel
     {
-        public class MailNotificationChannel : INotificationChannel
-        {
-            public void Send(Message message)
-            {
-                Console.WriteLine("Sending mail...");
-            }
-        }
-
         void Send(Message message);
     }
 }
