4c006a2285ace43eabe32365dda19e05a4d915fd
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-21 00:30:04 +0700

    6-5 update

--- 6. Interfaces/5.Polymorphism/INotficationChannel.cs
+++ 6. Interfaces/5.Polymorphism/INotficationChannel.cs
@@ -1,7 +1,18 @@
-﻿namespace Polymorphism
+﻿using System;
+
+namespace Polymorphism
 {
+    public class MailNotificationChannel : INotficationChannel
+    {
+        public void Send(Message message)
+        {
+            Console.WriteLine("Sending mail...");
+        }
+    }
+
     public interface INotficationChannel
     {
         void Send(Message message);
     }
 }
+
