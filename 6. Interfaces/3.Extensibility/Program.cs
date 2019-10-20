7d7d438ac49c4549d65dc89c1b23afaa0227b543
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 22:19:27 +0700

    6-3 Finish

--- 6. Interfaces/3.Extensibility/Program.cs
+++ 6. Interfaces/3.Extensibility/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Extensibility
+﻿using System;
+
+namespace Extensibility
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var dbMigrator = new DbMigrator(new ConsoleLogger("C:\\Projects\\log.txt"));
+            var dbMigrator = new DbMigrator(new FileLogger("C:\\Projects\\log.txt"));
             dbMigrator.Migrator();
         }
     }
