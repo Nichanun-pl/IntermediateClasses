29ef4a052c798edbed242d334b0b7aa4556c07d7
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 15:21:56 +0700

    3-3 Finish

--- 3. Association between Classes/3. Composition/Program.cs
+++ 3. Association between Classes/3. Composition/Program.cs
@@ -8,6 +8,10 @@
             
             var logger = new Logger();
             var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+            
+            installer.Install();
         }
     }
 }
