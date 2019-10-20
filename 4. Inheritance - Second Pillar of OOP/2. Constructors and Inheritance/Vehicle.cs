ed3fcb93b91a115cdbdf5c5f1620ed38202920db
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 17:26:22 +0700

    update

--- 4. Inheritance - Second Pillar of OOP/2. Constructors and Inheritance/Vehicle.cs
+++ 4. Inheritance - Second Pillar of OOP/2. Constructors and Inheritance/Vehicle.cs
@@ -6,14 +6,16 @@ namespace Constructors
     {
         private readonly string _registrationNumber;
 
-        public Vehicle()
-        {
-            Console.WriteLine("Vehicle is being initialized.");
-        }
+ //       public Vehicle()
+ //       {
+ //           Console.WriteLine("Vehicle is being initialized.");
+ //       }
 
         public Vehicle(string registrationNumber)
         {
             _registrationNumber = registrationNumber;
+
+            Console.WriteLine("Vehicle is being initialized. {0}", registrationNumber);
         }
     }
 }
