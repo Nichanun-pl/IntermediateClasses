e47e937b533f04c9a1936d9f1ecee9e555b4e586
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-21 00:35:17 +0700

    6-5 update

--- 6. Interfaces/5.Polymorphism/INotficationChannel.cs
+++ 6. Interfaces/5.Polymorphism/INotficationChannel.cs
@@ -2,14 +2,6 @@
 
 namespace Polymorphism
 {
-    public class SmsNotificationChannel : INotficationChannel
-    {
-        public void Send(Message message)
-        {
-            Console.WriteLine("Sending SMS...");
-        }
-    }
-
     public interface INotficationChannel
     {
         void Send(Message message);
