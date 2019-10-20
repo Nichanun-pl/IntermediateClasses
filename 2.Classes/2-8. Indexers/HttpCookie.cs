b765b3895ea746df661950ef0a7770576bcf9dd4
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 13:39:07 +0700

    update

--- 2.Classes/2-8. Indexers/HttpCookie.cs
+++ 2.Classes/2-8. Indexers/HttpCookie.cs
@@ -1,7 +1,21 @@
-﻿namespace Indexers
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexers
 {
     public class HttpCookie
     {
+        private Dictionary<string, string> _dictionary;
 
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        public string this[string key]
+        {
+            get {}
+            set {}
+        }
     }
 }
