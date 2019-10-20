25dc4e3a5f1214839693ea09f58a7c950f108c58
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 20:26:41 +0700

    5-2 update

--- 5. Polymorphism Third Pillar of OOP/2. Abstract Classes and Members/Program.cs
+++ 5. Polymorphism Third Pillar of OOP/2. Abstract Classes and Members/Program.cs
@@ -2,6 +2,17 @@
 
 namespace Abstract_Classes
 {
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
