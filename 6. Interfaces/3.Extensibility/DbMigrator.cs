63f348fed1430db3ed85af3d715a3954106e3f2e
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 22:00:41 +0700

    6-3 update

--- 6. Interfaces/3.Extensibility/DbMigrator.cs
+++ 6. Interfaces/3.Extensibility/DbMigrator.cs
@@ -4,6 +4,13 @@ namespace Extensibility
 {
     public class DbMigrator
     {
+        private readonly ILogger _logger;
+
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public void Migrator()
         {
             Console.WriteLine("Migrationg started at {0}", DateTime.Now);
