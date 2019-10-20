4eb46fa029bef2f05d8b7b6ebbe892b30528125b
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 18:28:58 +0700

    4-3 Finish

--- 4. Inheritance - Second Pillar of OOP/3. Upcasting and Downcasting/MainWindow.xaml.cs
+++ 4. Inheritance - Second Pillar of OOP/3. Upcasting and Downcasting/MainWindow.xaml.cs
@@ -28,6 +28,10 @@ namespace WpfApp1
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
+            if (button != null)
+            {
+                MessageBox.Show(button.ActualHeight.ToString());
+            }
 
             MessageBox.Show("Hello World!");
         }
