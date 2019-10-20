4ad861399adb213964627138312dea941f0c8237
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 21:53:13 +0700

    6-3 update

--- 6. Interfaces/3.Extensibility/Program.cs
+++ 6. Interfaces/3.Extensibility/Program.cs
@@ -2,6 +2,17 @@
 
 namespace Extensibility
 {
+    public class DbMigrator
+    {
+        public void Migrator()
+        {
+            Console.WriteLine("Migrationg started at {0}", DateTime.Now);
+            
+            //Details of migrating the database
+            
+            Console.WriteLine("Migrationg finished at {0}", DateTime.Now);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
