59312bb57eb94e66827b10f97f81f1b353ee4cf5
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 21:07:41 +0700

    6-2 create

---
+++ 6. Interfaces/2.Testability/Shipment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testability
+{
+    public class Shipment
+    {
+        internal DateTime shippingDate;
+
+        public float Cost { get; set; }
+        public DateTime ShippingDate { get; set; }
+    }
+}
