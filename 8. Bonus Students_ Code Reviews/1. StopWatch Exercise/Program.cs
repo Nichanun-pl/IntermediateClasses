795bc4d77fe7a8e958bc392c88bd4854833453c6
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-21 01:18:35 +0700

    8-1 Finish

--- 8. Bonus Students_ Code Reviews/1. StopWatch Exercise/Program.cs
+++ 8. Bonus Students_ Code Reviews/1. StopWatch Exercise/Program.cs
@@ -11,22 +11,20 @@ namespace CSharpInTermediate
 
             for (int i = 0; i < 2; i++)
             {
-                stopwatch.start(DateTime.Today.AddDays(1));
+                stopwatch.start();
 
-                for (int j = 0; j < 1000; j++)
-                {
-                    Thread.Sleep(1);
-                }
+                Thread.Sleep(1000);
 
-                //stopwatch.Start(DateTime.Now);
+                stopwatch.stop();
 
-                stopwatch.stop(DateTime.Today.AddYears(-1));
+                Console.WriteLine("Duration : " + stopwatch.GetInterval().ToString());
 
-                Console.WriteLine(stopwatch.GetInterval().ToString());
+                Console.WriteLine("Press Enter to run the stopwatch one more time");
                 Console.ReadLine();
             }
         }
     }
+
     public class Stopwatch
     {
         private DateTime _startTime;
@@ -38,24 +36,24 @@ namespace CSharpInTermediate
         {
             if (_running)
                 throw new InvalidOperationException("Stopwatch is already running");
-            
+
             _startTime = DateTime.Now;
             _running = true;
-        }
 
+        }
         public void stop()
         {
-            if (_running)
+            if (!_running)
                 throw new InvalidOperationException("Stopwatch is not running");
-            {
-                _endTime = DateTime.Now;
-                _running = false;
-            }
+
+            _endTime = DateTime.Now;
+            _running = false;
+
         }
 
         public TimeSpan GetInterval()
         {
-            return duration;
+            return _endTime - _startTime;
         }
     }
 }
