4de48b66f0a7edc4281f693690261d2e737393bc
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 16:07:04 +0700

    create

---
+++ 4. Inheritance - Second Pillar of OOP/1. Access Modifiers/Access Modifiers/Amazon/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Access_Modifiers
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promoto()
+        {
+            Console.WriteLine("Promoto logic changed.");
+        }
+
+    }
+}
