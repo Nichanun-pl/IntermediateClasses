6acaa4b8409f160e6ddd70d2d461b65d4939950d
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-20 21:35:48 +0700

    6-2 update

--- 6. Interfaces/2.Testability/OrderProcessorTests.cs
+++ 6. Interfaces/2.Testability/OrderProcessorTests.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testability.UnitTest;
 
 namespace Testability.UnitTest
 {
     [TestClass]
     public class OrderProcessorTests
     {
+        //METHODNAME_CONDITION_EXPECTION
+
         [TestMethod]
-        public void TestMethod1()
+        public void Process_OrderIsAlreatyShipptd_ThrowsAnException()
         {
+            var orderProcessor = new OrderProcessor(new FakeShippingCalculator());
+            var order = new Order();
+        }
+    }
+
+    public class FakeShippingCalculator : IshippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+            return 1;
         }
     }
 }
